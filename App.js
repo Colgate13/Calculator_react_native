bdf500c4bf48932e4fedc8e89a3fcd24aeeb31ef
Colgate13x <gabreilbarros13@gmail.com>
2021-02-01 12:40:54 -0300

    Estilizações prontas

--- App.js
+++ App.js
@@ -1,52 +1,84 @@
 import { StatusBar } from 'expo-status-bar';
-import React, {useState, useEffect }from 'react';
-import { StyleSheet, Text, TouchableHighlight, View } from 'react-native';
+import React,{useState, useEffect} from 'react';
+import { StyleSheet, Text, View, TouchableHighlight, TouchableOpacity } from 'react-native';
 import Botao from './Botao';
 
 export default function App() {
 
-  console.disableYellowBox = true;
-
+  console.disableYellowBox  = true;
   const [firstNumber, setFirstNumber] = useState(0);
   const [secondNumber, setSecondNumber] = useState(0);
-  const [sinal , setSinal ] = useState("");
+  const [sinal, setSinal] = useState("");
+
 
   const [stringCalculo, setStringCalculo] = useState("0");
 
   var numeros = [];
-  for(var i = 0; i <= 9; i++)
-  {
-    numeros.push(i);
+
+  for(var i = 0; i <= 9; i++){
+      numeros.push(i);
   }
 
+  function logicaCalculadora(){
+    //Lógica da calculadora.
+  }
+ 
   return (
-    <View style={{ flex: 1 }}>
-      <View style={styles.Topo}><Text style={{ fontSize: 24 }}>{stringCalculo}</Text></View>
-
-      <View style={{ flex: 1, flexDirection: 'row'}}> 
-        <TouchableHighlight style={{ width: '25%' }}><Text style={{ fontSize: 24, textAlign: 'center' }}>+</Text></TouchableHighlight>
-        <TouchableHighlight style={{ width: '25%' }}><Text style={{ fontSize: 24, textAlign: 'center' }}>-</Text></TouchableHighlight>
-
-        <TouchableHighlight style={{ width: '25%' }}><Text style={{ fontSize: 24, textAlign: 'center' }}>/</Text></TouchableHighlight>
-        <TouchableHighlight style={{ width: '25%' }}><Text style={{ fontSize: 24, textAlign: 'center' }}>*</Text></TouchableHighlight>
-
+    <View style={{flex:1,backgroundColor:'black'}}>
+      <StatusBar hidden />
+      <View style={styles.topo}><Text style={{fontSize:24,color:'white'}}>{stringCalculo}</Text></View>
+      
+      <View style={styles.view_TouchableOpacity}>
+        <TouchableOpacity style={styles.btnTouchaOpacity}>
+          <Text style={styles.text_view_TouchableOpacity}>+</Text></TouchableOpacity>
+        <TouchableOpacity style={styles.btnTouchaOpacity}>
+          <Text style={styles.text_view_TouchableOpacity}>-</Text>
+          </TouchableOpacity>
+        <TouchableOpacity style={styles.btnTouchaOpacity}>
+          <Text style={styles.text_view_TouchableOpacity}>/</Text>
+          </TouchableOpacity>
+        <TouchableOpacity style={styles.btnTouchaOpacity}>
+          <Text style={styles.text_view_TouchableOpacity}>*</Text>
+          </TouchableOpacity>
       </View>
-
-      <View>
-      {
-        numeros.map(function(e){
-          return (<Botao numeros={e}></Botao>)
-        })
-      }
+      
+      <View style={styles.view_botao}>
+          {
+            numeros.map(function(e){
+            return (<Botao logicaCalculadora={logicaCalculadora} numero={e}></Botao>);
+            })
+          }
       </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  Topo: {
-    padding: 10,
-    borderBottomColor: 'black',
-    borderBottomWidth: 2
-  },
+      topo:{
+        backgroundColor:'rgb(20,20,20)',
+        height:'16.6%',
+        justifyContent:'center',
+        paddingLeft:20
+      },
+      view_TouchableOpacity:{
+        flexDirection:'row',
+        height:'16.6%',
+        alignItems:'center'
+      },
+      btnTouchaOpacity:
+      {width:'25%',
+      backgroundColor:'rgb(20,20,20)',
+      justifyContent:'center',
+      alignItems:'center',
+      height:'100%'
+    },
+    text_view_TouchableOpacity:{
+      fontSize:24,color:'white'
+    },
+    view_botao:{
+      flexDirection:'row',
+      flexWrap:'wrap',
+      borderTopColor:'black',
+      borderTopWidth:2,height:'66.8%'
+    }
 });
--- Botao.js
+++ Botao.js
@@ -1,8 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import React, {useState, useEffect }from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{useState, useEffect} from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
-export default function Botao(props) {
+export default function Botao(props){
 
-    return (<View><Text>{props.numeros}</Text></View>);
+        return (
+        <View style={styles.view_father}>
+        <TouchableOpacity onPress={()=>props.logicaCalculadora()} style={styles.btn_TouchableOpacity} 
+        ><Text style={styles.text_TouchableOpacity}>{props.numero}</Text>
+        </TouchableOpacity>
+        </View>
+        );
 }
+const styles = StyleSheet.create({
+    view_father:{
+        backgroundColor:'black',
+        borderColor:'white',
+        borderWidth:1,
+        width:'33.3%',
+        height:'25%'
+    },
+    btn_TouchableOpacity:{
+        width:'100%',
+        height:'100%',
+        justifyContent:'center',
+        alignItems:'center'
+    },
+    text_TouchableOpacity:{
+        fontSize:24,
+        color:'white'
+    },
+});
